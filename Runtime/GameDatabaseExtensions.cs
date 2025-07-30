2414a947befba9de839142f4d96b962b642062e7
Mefodei <mefodei.link@gmail.com>
2025-07-30 09:44:36 +0700

    2025.0.9

---
+++ Runtime/GameDatabaseExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace Game.Modules.game.packages.unigame.gamedb.Runtime
+{
+    using Code.DataBase.Runtime.Abstract;
+    using Cysharp.Threading.Tasks;
+    using UniGame.Core.Runtime;
+
+    public static class GameDatabaseExtensions
+    {
+        public static async UniTask<TAsset> LoadAssetAsync<TAsset>(this IGameDatabase database, string assetId, ILifeTime lifeTime)
+        {
+            var result = await database.LoadAsync<TAsset>(assetId,lifeTime);
+            return result.Result;
+        }
+    }
+}
